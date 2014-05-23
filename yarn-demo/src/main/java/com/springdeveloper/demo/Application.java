f6c68ebab7dc48e5fd115879839b7e6b2b5b41bd
Thomas Risberg <trisberg@gopivotal.com>
2014-05-23 17:41:05 -0400

    Cleanup

--- yarn-demo/src/main/java/com/springdeveloper/demo/Application.java
+++ yarn-demo/src/main/java/com/springdeveloper/demo/Application.java
@@ -2,15 +2,12 @@ package com.springdeveloper.demo;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.yarn.annotation.OnContainerStart;
 import org.springframework.yarn.annotation.YarnComponent;
-import org.springframework.yarn.config.annotation.configuration.SpringYarnContainerConfiguration;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -18,8 +15,6 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("*** RUNNING");
-        SpringYarnContainerConfiguration c = new SpringYarnContainerConfiguration();
-
         SpringApplication.run(Application.class, args);
     }
 
